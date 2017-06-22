77bb607e450c5facea4417e8aa7bbcb85a0bcfd2
Tarun <none>
2017-06-22 10:37:27 -0600

    Added New Assessment and jquery

--- IRC/views.py
+++ IRC/views.py
@@ -11,3 +11,13 @@ def index(request):
 		context = {}
 	
 	return render(request, 'IRC/index.html', context)
+
+
+def newAssessment(request):
+	if request.user.is_authenticated:
+		assessmentList = Assessment.objects.filter(assessor=request.user.id)
+		context = {}
+	else:
+		context = {}
+
+	return render(request, 'IRC/newAssessment.html', context)
